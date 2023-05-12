a528023cf619407b32b761e3debf8de585e7249c
Sondre Grønås <sondre.gronas@gmail.com>
2023-05-12 17:09:07 +0200

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,38 +1,56 @@
 import discord
+from time import sleep
+
+intents = discord.Intents.default()
+intents.message_content = True  # Må aktiveres under Privileged Gateway Intents på https://discord.com/developers/applications/
+                                # (Message Content Intent)
+client = discord.Client(intents=intents)
+
+"""
+Under her legger du til events og kommandoer som botten skal kunne utføre.
+on_message er den mest brukte eventen, og kjøres hver gang botten mottar en melding.
+"""
 
 
-class MinBot(discord.Client):
-    async def on_ready(self):
-        """on_ready kjøres når botten er klar til å brukes."""
-        print(f"Logget inn som {self.user}")
-
-    async def on_message(self, message):
-        """on_message kjøres når botten mottar en melding."""
-        # Sjekker om meldingen er fra boten selv, for å unngå at den svarer på seg selv.
-        if message.author == self.user:
-            return
-
-        if message.content == "ping":
-            await message.channel.send("pong")
-
-        # Samme syntaks kan repeteres for flere kommandoer
-        if message.content.lower() == "hei":
-            await message.channel.send(f"Hei {message.author.mention}!")
-
-    async def on_member_join(self, member):
-        """on_member_join kjøres når en ny bruker blir med i serveren."""
-        await member.send("Velkommen til serveren!")
-
-    # Det finnes mange flere events, se https://discordpy.readthedocs.io/en/latest/api.html#event-reference
+@client.event
+async def on_ready():
+    """on_ready kjøres når botten er klar til å brukes."""
+    print(f"Logget inn som {client.user}")
 
 
-if __name__ == "__main__":
-    """Gir botten tillatelse til å lese meldinger, defineres når du lager en bot på discord.com/developers"""
-    intents = discord.Intents.default()
-    intents.message_content = True
+@client.event
+async def on_message(message):
+    """on_message kjøres når botten mottar en melding."""
+    text = message.content
+    user = message.author
 
-    client = MinBot(intents=intents)
-    # Leser token fra token.txt, som er lagret i .gitignore for å unngå at den blir lastet opp til GitHub
-    # Behandle den som et passord, ikke del den med andre! (Generer en ny token hvis du har delt den med noen)
-    # Husk å kjøre src\setup.py før du kjører denne filen, for å lage token.txt
-    client.run(open("token.txt", "r").read())
+    # Sjekker om meldingen er fra boten selv, for å unngå at den svarer på seg selv.
+    if user == client.user:
+        return
+
+    if text == "ping":
+        await message.channel.send("pong")
+
+    # Samme syntaks kan repeteres for flere kommandoer
+    if text.lower() == "hei":
+        await message.channel.send(f"Hei {user.mention}!")
+
+
+# Det finnes flere ulike events man kan bruke til ulike formål,
+# se https://discordpy.readthedocs.io/en/latest/api.html#event-reference
+
+
+# Til slutt; kjør botten med token fra token.txt (med litt tips og feilsøking)
+# Du kan ignorere dette.
+if __name__ == '__main__':
+    print('Starter botten.')
+    try:
+        client.run(open("token.txt", "r").read())
+    except discord.errors.PrivilegedIntentsRequired:
+        print('OBS! Din bot mangler "Message Content Intent", legg til denne \n'
+              'på https://discord.com/developers/applications/ (Under Privileged Gateway Intents)')
+    except discord.errors.LoginFailure:
+        print('Kunne ikke logge på botten, bruker du riktig token i token.txt?')
+    finally:
+        # Vent 5 sekunder før EventLoop lukkes, større og mindre lesbar feilmelding.
+        sleep(5)
