6d3ce30632250e77d406d9dee38490cc84a67901
Sondre Grønås <sondre.gronas@gmail.com>
2023-05-12 16:21:07 +0200

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
     open('token.txt', 'w').write(input('Lim inn din token: '))
-    print('token.txt er nå generert, du skal nå kunne kjøre main.py dersom oppgitt token er gyldig.'
+    print('token.txt er nå generert, du skal nå kunne kjøre main.py dersom oppgitt token er gyldig.')
