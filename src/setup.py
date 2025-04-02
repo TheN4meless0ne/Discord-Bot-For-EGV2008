2663dadb3d9d418ab8a50d0acef5df9455355944
TheN4meless0ne <thenmelessne@gmail.com>
2025-04-02 08:33:07 +0200

    rm setup.py

--- src/setup.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    open('token.txt', 'w').write(input('Lim inn din token: '))
-    print('token.txt er nå generert, du skal nå kunne kjøre main.py dersom oppgitt token er gyldig.')
-    print('Husk at botten må være lagt til en av dine servere på forhånd før den dukker opp.')
-    print('(Dette gjør du på discord.com/developers -> din bot -> OAuth2 -> Url Generator -> bot -> Administrator -> følg url)')
